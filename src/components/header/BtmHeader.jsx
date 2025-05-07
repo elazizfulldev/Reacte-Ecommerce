f32df76453eedd0795f0b9139456afd7dc87c6c6
elazizfulldev <elazizfulldev@gmail.com>
2025-05-07 18:23:00 +0100

    Implemented bottom navbar using useEffect, useState, and useLocation hooks; added CSS styling.

--- src/components/header/BtmHeader.jsx
+++ src/components/header/BtmHeader.jsx
@@ -1,7 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { IoMdMenu } from "react-icons/io"
+import { IoMdArrowDropdown } from "react-icons/io"
+import { Link, useLocation} from 'react-router-dom'
+import { PiSignInBold } from "react-icons/pi";
+import { FaUserPlus } from "react-icons/fa";
+
+const NavLinks = [
+    {title : "Home" , link : "/"},
+    {title : "About" , link : "/about"},
+    {title : "Accessories" , link : "/accessories"},
+    {title : "Blog" , link : "/blog"},
+    {title : "Contact" , link : "/contact"},
+
+]
 
 export default function BtmHeader() {
+
+    const location = useLocation()
+
+    const [categories , setCategories] = useState([])
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products/categories')
+        .then((res) => res.json())
+        .then((data)=> setCategories(data))
+    },[])
+
+    console.log(categories)
+
+
   return (
-    <div>BtmHeader</div>
+    <div className="btm-header">
+
+        <div className="container">
+            <nav className="nav">
+
+                <div className="category-nav">
+                    <div className="category-btn">
+                        <IoMdMenu />
+                        <p>Browse Category</p>
+                        <IoMdArrowDropdown />
+                    </div>
+                    <div className="category-nav-list">
+                        {categories.map((category) =>(
+                            <Link to={category.slug}>{category.name}</Link>
+                        ))}
+
+                    </div>
+
+
+                </div>
+
+                <div className="nav-links">
+
+                    {NavLinks.map((item)=>(
+
+                        <Link to={item.link} className={location.pathname === item.link ? "active" : ""}> {item.title} </Link>
+
+                    ))}
+                    
+
+                </div>
+
+
+
+            </nav>
+            <div className="sign-regs-icon">
+                <Link to="/"><PiSignInBold /></Link>
+                <Link to="/"><FaUserPlus /></Link>
+
+            </div>
+        </div>
+
+    </div>
   )
 }
