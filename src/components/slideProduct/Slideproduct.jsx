5a75b1d5c1933173feaaced355424a791a27fd00
elazizfulldev <elazizfulldev@gmail.com>
2025-05-09 10:15:46 +0100

    Add SlideProduct component with Swiper integration

--- src/components/slideProduct/Slideproduct.jsx
+++ src/components/slideProduct/Slideproduct.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Product from './Product'
 import './slideProduct.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Pagination, Navigation } from 'swiper/modules';
 
 export default function SlideProduct() {
   return (
@@ -12,7 +18,17 @@ export default function SlideProduct() {
 
                 
             </div>
-            <Product/>
+            
+            <Swiper slidesPerView={5} spaceBetween={30} navigation={true} modules={[Pagination, Navigation]} className="mySwiper">
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+              <SwiperSlide><Product/></SwiperSlide>
+            
+            </Swiper>
 
         </div>
    
