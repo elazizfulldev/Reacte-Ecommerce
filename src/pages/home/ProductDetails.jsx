b6d95b9702bdd4dee62e1080d71d97313f108619
elazizfulldev <elazizfulldev@gmail.com>
2025-05-19 12:51:57 +0100

    creat the product page structer

--- src/pages/home/ProductDetails.jsx
+++ src/pages/home/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { TiStarFullOutline ,TiStarHalfOutline} from "react-icons/ti";
 
 export default function ProductDetails() {
 
@@ -36,8 +37,49 @@ export default function ProductDetails() {
 
     
   return (
-    <div>
-        <h2>{product.title}</h2>
+    <div className="item_details">
+        <div className="container">
+            <div className="img-item">
+                <div className="big-img">
+                    <img src={product.images[0]} alt={product.title} />
+
+                </div>
+                <div className="sm-img">
+                    {
+
+                        product.images.map((img , index)=>(
+
+                            <img key={index} src = {img} alt={product.title}/>
+
+                        ))
+
+                    }
+
+                </div>
+            </div>
+
+            <div className="details-item">
+                <h1 className='name'>{product.title}</h1>
+                <div className="stars">
+                    <TiStarFullOutline />
+                    <TiStarFullOutline />
+                    <TiStarFullOutline />
+                    <TiStarFullOutline />
+                    <TiStarHalfOutline />
+
+                </div>
+
+                <p className="price">$ {product.price}</p>
+                <h5>Availability : <span>{product.availabilityStatus}</span></h5>
+                <h5>Brand : <span>{product.brand}</span></h5>
+                <p className='desc'>{product.description}</p>
+                <h5>Hurry Up! Only  <span>{product.stock}</span> Product Left In Stock . </h5>
+                <h5>Category : <span>{product.category}</span></h5>
+
+            </div>
+
+        </div>
+
     </div>
   )
 }
