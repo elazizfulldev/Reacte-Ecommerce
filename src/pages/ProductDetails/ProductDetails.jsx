ff6e75e154da0e2d57c980a102b286a66fe8ae5d
elazizfulldev <elazizfulldev@gmail.com>
2025-05-23 17:33:58 +0100

    create and design product page slider by category & create loading skletonpage for it

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { TiStarFullOutline ,TiStarHalfOutline} from "react-icons/ti";
 import { FaCartArrowDown , FaShare , FaHeart , FaRegHeart} from "react-icons/fa";
 import './ProductDetails.css'
+import SlideProduct from '../../components/slideProduct/slideProduct';
+import ProductDetailsLOading from './ProductDetailsLOading';
 
 export default function ProductDetails() {
 
@@ -11,6 +13,7 @@ export default function ProductDetails() {
     const [loading , setloading] = useState(true)
     const [relatedProduct , setRelatedProduct] = useState([])
     const [loadingRelatedProduct , setLoadingRelatedProduct] = useState(true)
+    const [selectedIndex, setSelectedIndex] = useState(0);
 
     console.log(id )
 
@@ -34,6 +37,8 @@ export default function ProductDetails() {
         fetchproducts()
     },[id])
 
+    /*===== this useEffect =======*/
+
     useEffect(()=>{
 
         if(!product) return
@@ -49,63 +54,74 @@ export default function ProductDetails() {
     },[product?.category])
 
     console.log(product)
+    console.log(relatedProduct)
 
-    if(loading) return <p>Loading.....</p>
+    if(loading) return <ProductDetailsLOading/> 
     if(product.id != Number(id)) return <p>Product not found</p>
 
     
   return (
-    <div className="item-details">
-        <div className="container">
-            <div className="img-item">
-                <div className="big-img">
-                    <img id='big-img' src={product.images[0]} alt={product.title} />
+    
+    <div>
+        <div className="item-details">
+            <div className="container">
+                <div className="img-item">
+                    <div className="big-img">
+                        <img id='big-img' src={product.images[0]} alt={product.title} />
 
-                </div>
-                <div className="sm-img">
-                    {
+                    </div>
+                    <div className="sm-img">
+                        {
 
-                        product.images.map((img , index)=>(
+                            product.images.map((img , index)=>(
 
-                            <img key={index} src = {img} alt={product.title} onClick={()=>document.getElementById("big-img").src = img } />
+                                <img key={index} src = {img} alt={product.title} onClick={()=>{document.getElementById("big-img").src = img; setSelectedIndex(index) }} className={selectedIndex === index ?"min-img-border":"min-img-border-none"} />
 
-                        ))
+                            ))
 
-                    }
+                        }
 
-                </div>
-            </div>
-
-            <div className="details-item">
-                <h1 className='name'>{product.title}</h1>
-                <div className="stars">
-                    <TiStarFullOutline />
-                    <TiStarFullOutline />
-                    <TiStarFullOutline />
-                    <TiStarFullOutline />
-                    <TiStarHalfOutline />
-
+                    </div>
                 </div>
 
-                <p className="price">$ {product.price}</p>
-                <h5>Availability : <span>{product.availabilityStatus}</span></h5>
-                <h5>Brand : <span>{product.brand}</span></h5>
-                <p className='desc'>{product.description}</p>
-                <h5 className='stock'>Hurry Up! Only  <span>{product.stock}</span> Product Left In Stock . </h5>
-                <h5 className='category'>Category : <span>{product.category}</span></h5>
-                <button className='btn'> <FaCartArrowDown /> Add to cart</button>
+                <div className="details-item">
+                    <h1 className='name'>{product.title}</h1>
+                    <div className="stars">
+                        <TiStarFullOutline />
+                        <TiStarFullOutline />
+                        <TiStarFullOutline />
+                        <TiStarFullOutline />
+                        <TiStarHalfOutline />
 
-                <div className="icons">
-                    <span><FaRegHeart /></span>
-                    <span><FaShare /></span>
+                    </div>
+
+                    <p className="price">$ {product.price}</p>
+                    <h5>Availability : <span>{product.availabilityStatus}</span></h5>
+                    <h5>Brand : <span>{product.brand}</span></h5>
+                    <p className='desc'>{product.description}</p>
+                    <h5 className='stock'>Hurry Up! Only  <span>{product.stock}</span> Product Left In Stock . </h5>
+                    <h5 className='category'>Category : <span>{product.category}</span></h5>
+                    <button className='btn'> <FaCartArrowDown /> Add to cart</button>
+
+                    <div className="icons">
+                        <span><FaRegHeart /></span>
+                        <span><FaShare /></span>
+                    </div>
+                    
+                    
+
                 </div>
-                
-                
 
             </div>
 
         </div>
 
+        {loadingRelatedProduct ? (
+            <p>Loading...</p>
+        ):(
+            <SlideProduct key={product.category} title={product.category.replace("-"," ")}  data={relatedProduct.products} />
+        )}
     </div>
+    
   )
 }
---
+++ src/pages/ProductDetails/ProductDetailsLOading.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './ProductDetails.css'
+
+export default function ProductDetailsLOading() {
+    return (
+        <div className='loading-item'>
+            <div className="item-details">
+                <div className="container">
+                    <div className="img-item skeleton">
+                        
+                    </div>
+
+                    <div className="details-item">
+
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                        <h5 className='loading-text-detailes skeleton'></h5>
+                      
+                        
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
